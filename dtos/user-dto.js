60d98a4bae7ae579573c60f7fbd910e3b2c60b9d
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-09-28 15:32:50 +0530

    Update user dto.

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -3,6 +3,8 @@ class UserDto {
     this.id = user._id;
     this.username = user.username;
     this.email = user.email;
+    this.forms = user.forms;
+    this.totalForms = user.totalForms;
     this.createdAt = user.createdAt;
   }
 }
